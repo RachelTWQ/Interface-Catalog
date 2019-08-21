63fc98cc84eb01d105c16b065a9f7b232cb5d90a
RachelTWQ <racheltang15@hotmail.com>
2019-08-21 14:23:07 -0400

    add selector to todo list

--- src/App.js
+++ src/App.js
@@ -21,11 +21,15 @@ function App() {
     setTodos(medium); 
   }
 
+  function removeToDo(event) {
+    console.log(event.target.id)
+  }
+
   return (
     <div className="App">
       <main>
         <h3>To Do List</h3>
-        {todos && todos.length ? todos.map(el => (<p key={el.time}><span>{el.content}</span></p>)) : (<p>Nothing to do Yay!</p>)}
+        {todos && todos.length ? todos.map(el => (<p key={el.time}><span id={el.time} onClick={removeToDo}>{el.content}</span></p>)) : (<p>Nothing to do Yay!</p>)}
         <input type="text" placeholder="What to do next?" onChange= {(e) => setEntry(e.currentTarget.value)} onKeyDown={handleEnter} />
       </main>
     </div>
